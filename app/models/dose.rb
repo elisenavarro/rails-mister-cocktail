c91e81da19ef1152e3d3f6298fa652ca85b8959f
Elise Navarro <elise.marie.navarro@gmail.com>
2020-01-20 19:48:09 -0700

    UPDATE: Added Reviews to Models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -4,18 +4,3 @@ class Dose < ApplicationRecord
   validates :description, presence: true
   validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 end
-
-# # retrieve the cocktail
-# # @dose: {description: "fefefe", cocktail_id: 12, ingredient_id: 343}
-# # @dose.cocktail
-# def cocktail
-#   Cocktail.find(self.cocktail_id)
-# end
-
-
-# # set the cocktail
-# # @dose: {description: "fefefe", cocktail_id: 12, ingredient_id: 343}
-# # @dose.cocktail = @cocktail
-# def cocktail=(cocktail)
-#   self.cocktail_id = cocktail.id
-# end
